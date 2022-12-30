45aa874000a64dae23f4e36e26044c928b2a7240
Bitzilla22 <102140274+Bitzilla22@users.noreply.github.com>
2022-12-30 20:33:58 +0100

    init

---
+++ app.js
@@ -0,0 +1,55 @@
+const { twitterBearer, twitterClient } = require("./client.js");
+const express = require("express");
+const {readFile} = require("fs/promises");
+const CronJob = require("cron").CronJob;
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+	console.log(`Server started on PORT ${port}`);
+});
+
+const readQuery = async () => {
+	const res = await readFile("./query.txt", "utf8")
+	return res
+};
+
+const readQuoteTweet = async () => {
+	const res = await readFile("./quote_tweet.txt", "utf8")
+	return res
+};
+
+const getTweets = async () => {
+	const queryRes = await readQuery()
+	const quoteMessage = await readQuoteTweet()
+
+	const result = await twitterClient.v2.get("tweets/search/recent", {
+		query: queryRes,
+		max_results: 10,
+		start_time: new Date(Date.now() - (process.env.TIME_INTERVAL * 60000)).toISOString()
+	});
+	const x = result.data.map(d => d.id)
+
+	console.log(x);
+	x.forEach(async (d) => {
+		await quoteTweet(d)
+	})
+};
+
+const quoteTweet = async (id) => {
+	const { data: createdTweet } = await twitterClient.v2.tweet(
+		"Testing Support",
+		{
+			quote_tweet_id: id,
+		}
+	);
+	console.log("Tweet", createdTweet.id, ":", createdTweet.text);
+};
+// getTweets()
+
+// const cronTweet = new CronJob(`* 30 * * * *`, async () => {
+const cronTweet = new CronJob(process.env.CRON_SETTINGS, async () => {
+	getTweets();
+});
+
+cronTweet.start();
---
+++ client.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { TwitterApi } = require("twitter-api-v2");
+
+const client = new TwitterApi({
+	appKey: process.env.API_KEY,
+	appSecret: process.env.API_SECRET,
+	accessToken: process.env.ACCESS_TOKEN,
+	accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+const bearer = new TwitterApi(process.env.BEARER_TOKEN);
+
+const twitterClient = client.readWrite;
+const twitterBearer = bearer.readOnly;
+
+module.exports = { twitterBearer, twitterClient };
